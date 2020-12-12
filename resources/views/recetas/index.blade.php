aae7cf205bf0b09e69d9af7feef04e5e2f0e916b
Cesar Acual <chechaacual@gmail.com>
2020-12-11 19:20:53 -0600

    Preparando la vista para recetas

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Recetas</h1>
+    <h2 class="text-center mb-5">Recetas</h2>
+
+    <div class="col-md-10 mx-auto bg-white p-3">
+        <table class="table">
+            <thead class="bg-primary text-light">
+                <tr>
+                    <th scole="col">Título</th>
+                    <th scole="col">Categoría</th>
+                    <th scole="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Pizza</td>
+                    <td>Pizzas</td>
+                    <td></td>
+            </tbody>
+        </table>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/recetas', 'RecetaController@index');
+
 Auth::routes();
