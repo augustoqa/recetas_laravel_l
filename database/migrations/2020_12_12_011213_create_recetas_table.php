3b58107fcd3c239dfef236facf7b3664f61d1df7
Cesar Acual <augustoacual@gmail.com>
2020-12-16 15:36:43 -0600

    Creando el Resto Campos para recetas

--- database/migrations/2020_12_12_011213_create_recetas_table.php
+++ database/migrations/2020_12_12_011213_create_recetas_table.php
@@ -13,9 +13,26 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+        Schema::create('categoria_recetas', function(Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
+            $table->text('ingredientes');
+            $table->text('preparacion');
+            $table->string('imagen');
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->comment('El usuario que crea la receta');
+            $table->foreignId('categoria_id')
+                ->references('id')
+                ->on('categoria_recetas')
+                ->comment('La categoría de la receta');
             $table->timestamps();
         });
     }
@@ -27,6 +44,7 @@ class CreateRecetasTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('categoria_recetas');
         Schema::dropIfExists('recetas');
     }
 }
