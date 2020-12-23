5763479a4b098047e222d54dc6538599cc161af4
Cesar Acual <augustoacual@gmail.com>
2020-12-23 17:09:51 -0600

    Se crea la relacion para mostrar el Autor en lavista de una Receta

--- resources/views/recetas/show.blade.php
+++ resources/views/recetas/show.blade.php
@@ -16,8 +16,7 @@
 
 			<p>
 				<span class="font-weight-bold text-primary">Autor:</span>
-				{{-- TODO:: mostrar el usuario --}}
-				{{ $receta->user_id }}
+				{{ $receta->autor->name }}
 			</p>
 
 			<p>
