0b23d53f4ef87aa9d93ccb0e13d2cdae8dc19781
Cesar Acual <augustoacual@gmail.com>
2021-01-11 22:38:43 -0600

    Paginar Recetas

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -32,5 +32,9 @@
                 @endforeach
             </tbody>
         </table>
+
+        <div class="col-12 mt-4 justify-content-center d-flex">
+            {{ $recetas->links() }}
+        </div>
     </div>
 @endsection
