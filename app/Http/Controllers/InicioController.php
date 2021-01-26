42937f3109b67f075dbb038a5069f42485b63b84
Cesar Acual <augustoacual@gmail.com>
2021-01-25 18:11:12 -0600

    Mostrar últimas recetas en la página principal

---
+++ app/Http/Controllers/InicioController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Receta;
+use Illuminate\Http\Request;
+
+class InicioController extends Controller
+{
+    public function index()
+    {
+        // Obtener las recetas recientes
+        $nuevas = Receta::latest()->take(5)->get();
+
+        return view('inicio.index', \compact('nuevas'));
+    }
+}
