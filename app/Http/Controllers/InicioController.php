265ab2787e98113235870ba14f2cec95e5169b64
Cesar Acual <augustoacual@gmail.com>
2021-01-27 16:39:45 -0600

    Obtener las recetas por su categoría

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Receta;
+use App\CategoriaReceta;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
@@ -11,6 +13,23 @@ class InicioController extends Controller
     {
         // Obtener las recetas recientes
         $nuevas = Receta::latest()->take(5)->get();
+
+        $categorias = CategoriaReceta::all();
+        
+        // Agrupar las recetas por categoria
+        $recetas = [];
+
+        foreach ($categorias as $categoria) {
+            $recetas[ Str::slug($categoria->nombre) ][] = 
+                Receta::where('categoria_id', $categoria->id)
+                    ->take(3)
+                    ->get();
+        }
+
+        return $recetas;
+
+        // Recetas por categoría
+        $mexicana = Receta::where('categoria_id', 1)->get();
 
         return view('inicio.index', \compact('nuevas'));
     }
